27d3cc6458520fb64798bd6b81fa6619998ccabb
Bernardo Vieira <bernardo@impactmarket.com>
2021-10-02 19:24:27 +0100

    improve body typography

--- packages/ui/src/typography/Body/index.tsx
+++ packages/ui/src/typography/Body/index.tsx
@@ -1,33 +1,46 @@
 import React from 'react'
-import { View, Text, TextProps } from 'react-native'
+import { Text, TextProps, TextStyle, StyleProp } from 'react-native'
 
-export default function Body(props: { variant?: 'micro' | 'small' | 'normal' | 'large', textProps?: TextProps, children?: string }) {
-    const variantStyle = () => {
-        const { variant } = props;
-        if (variant === 'micro') {
-            return {
-                fontSize: 10,
-                lineHeight: 19,
-            }
-        }
-        else if (variant === 'small') {
-            return {
-                fontSize: 12,
-                lineHeight: 20,
-            }
-        }
-        else if (variant === 'large') {
-            return {
-                fontSize: 18,
-                lineHeight: 32,
-            }
-        }
+interface BodyProps extends TextProps {
+    variant?: 'micro' | 'small' | 'normal' | 'large',
+    children?: JSX.Element | string,
+}
+
+export function bodyStyle(variant?: 'micro' | 'small' | 'normal' | 'large'): StyleProp<TextStyle> {
+    const commonStyle: StyleProp<TextStyle> = {
+        fontFamily: 'Inter_400Regular',
+        textAlignVertical: 'center',
+    };
+    if (variant === 'micro') {
         return {
-            fontSize: 15,
-            lineHeight: 24,
+            ...commonStyle,
+            fontSize: 10,
+            lineHeight: 19,
         }
     }
+    else if (variant === 'small') {
+        return {
+            ...commonStyle,
+            fontSize: 12,
+            lineHeight: 20,
+        }
+    }
+    else if (variant === 'large') {
+        return {
+            ...commonStyle,
+            fontSize: 18,
+            lineHeight: 32,
+        }
+    }
+    return {
+        ...commonStyle,
+        fontSize: 15,
+        lineHeight: 24,
+    }
+}
+
+export default function Body(props: BodyProps) {
     return (
-        <Text {...props.textProps} style={[variantStyle(), { fontFamily: 'Inter_400Regular' }, props.textProps?.style]}>{props.children}</Text>
+        <Text {...props} style={[bodyStyle(props.variant), props.style]} />
     )
 }
