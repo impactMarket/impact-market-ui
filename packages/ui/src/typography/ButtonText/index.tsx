1ff89a0d21a9417f13fb5d2446ecbd31bbb5cacd
Bernardo Vieira <bernardo@impactmarket.com>
2021-10-06 13:03:39 +0100

    add buttontext typography

---
+++ packages/ui/src/typography/ButtonText/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text, TextProps, TextStyle, StyleProp } from 'react-native'
+
+export default function ButtonText(props: TextProps & { children: JSX.Element | string }) {
+    return (
+        <Text {...props} style={[props.style, { fontSize: 15, lineHeight: 28, fontFamily: 'Manrope_400Regular' }]} />
+    )
+}
--- packages/ui/src/typography/index.ts
+++ packages/ui/src/typography/index.ts
@@ -1,5 +1,6 @@
 import Body from './Body';
+import ButtonText from './ButtonText';
 import Title from './Title';
 import Heading from './Heading';
 
-export { Body, Title, Heading };
+export { Body, ButtonText, Title, Heading };
