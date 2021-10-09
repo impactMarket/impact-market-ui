808f72cabf2db2869c35e52aca0a6c4b636064f4
Bernardo Vieira <bernardo@impactmarket.com>
2021-10-09 12:00:44 +0100

    fix buttontext font

--- packages/ui/src/typography/ButtonText/index.tsx
+++ packages/ui/src/typography/ButtonText/index.tsx
@@ -3,6 +3,6 @@ import { Text, TextProps, TextStyle, StyleProp } from 'react-native'
 
 export default function ButtonText(props: TextProps & { children: JSX.Element | string }) {
     return (
-        <Text {...props} style={[props.style, { fontSize: 15, lineHeight: 28, fontFamily: 'Manrope_400Regular' }]} />
+        <Text {...props} style={[props.style, { fontSize: 15, lineHeight: 28, fontFamily: 'Manrope_600SemiBold' }]} />
     )
 }
