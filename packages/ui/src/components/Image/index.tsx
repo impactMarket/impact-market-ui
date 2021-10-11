a3d3d486842fa74573e3bc1db36e90c8550a1ae5
Bernardo Vieira <bernardo@impactmarket.com>
2021-10-11 10:24:55 +0100

    fix thrown error by image

--- packages/ui/src/components/Image/index.tsx
+++ packages/ui/src/components/Image/index.tsx
@@ -11,21 +11,25 @@ export default class Image extends Component<ImageProps, { error: boolean }> {
         }
     }
 
-    onError = () => {
+    onError = (error) => {
         this.setState({ error: true });
+        this.props.onError?.(error);
     }
 
     render() {
         const { error } = this.state;
         const { height, width, style } = this.props;
         if (error) {
-            return <View style={[{
-                height,
-                width,
-                backgroundColor: colors.background.secondary,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }, style]}>
+            return <View style={[
+                {
+                    height,
+                    width,
+                    backgroundColor: colors.background.secondary,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                },
+                style
+            ]}>
                 <ImageErrorIcon />
             </View>
         }
